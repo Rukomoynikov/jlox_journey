67670e98e4b3216babbc746d6338203cf45054b9
Max Rukomoynikov <rukomoynikov@gmail.com>
2024-12-16 13:48:01 +0000

    Visitor pattern

--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -32,11 +32,16 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, tokens);
+
     for(String token : tokens) {
       String className = token.split(":")[0].trim();
       String fields = token.split(":")[1].trim();
       defineType(writer, baseName, className, fields);
     }
+
+    writer.println();
+    writer.println(" abstract <R> R accept(Visitor<R> visitor);");
 
     writer.println("}");
     writer.close();
@@ -60,10 +65,31 @@ public class GenerateAst {
 
     writer.println("    }");
 
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
+    writer.println("    }");
+
     // Fields
     writer.println();
     for(String field : fields) {
       writer.println("    final " + field + ";");
+    }
+
+    writer.println("  }");
+    writer.println();
+  }
+
+  private static void defineVisitor(PrintWriter writer,
+                             String baseName,
+                             List<String> tokens) {
+    writer.println("  interface Visitor<R> {");
+
+    for(String token: tokens) {
+      String typeName = token.split(" ")[0].trim();
+
+      writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
     }
 
     writer.println("  }");
